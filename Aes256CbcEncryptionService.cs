39f6f5585a3928ea0cdc54c753d77c8a18016cbd
savaged <david.savage@assure-consulting.co.uk>
2021-04-07 13:22:08 +0100

    init

---
+++ Aes256CbcEncryptionService.cs
@@ -0,0 +1,89 @@
+﻿using System.Security.Cryptography;
+using System.IO;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace CryptoEmulator
+{
+    public class Aes256CbcEncryptionService
+    {
+        private static readonly Encoding _encoding = Encoding.UTF8;
+        private readonly RijndaelManaged _aes;
+
+        public Aes256CbcEncryptionService(string key)
+        {
+            _aes = new RijndaelManaged
+            {
+                KeySize = 256,
+                BlockSize = 128,
+                Padding = PaddingMode.PKCS7,
+                Mode = CipherMode.CBC,
+                Key = _encoding.GetBytes(key)
+            };
+        }
+
+		public string Encrypt(string plainText)
+		{
+			try
+			{
+                _aes.GenerateIV();
+
+				ICryptoTransform AESEncrypt = _aes.CreateEncryptor(_aes.Key, _aes.IV);
+				byte[] buffer = _encoding.GetBytes(plainText);
+
+				var encryptedText = Convert.ToBase64String(AESEncrypt.TransformFinalBlock(buffer, 0, buffer.Length));
+
+				var mac = "";
+
+				mac = BitConverter.ToString(HmacSHA256(Convert.ToBase64String(_aes.IV) + encryptedText)).Replace("-", "").ToLower();
+
+				var keyValues = new Dictionary<string, object>
+				{
+					{ "iv", Convert.ToBase64String(_aes.IV) },
+					{ "value", encryptedText },
+					{ "mac", mac },
+				};
+
+				return Convert.ToBase64String(_encoding.GetBytes(JsonConvert.SerializeObject(keyValues)));
+			}
+			catch (Exception e)
+			{
+			    throw new Exception("Error encrypting: " + e.Message);
+			}
+		}
+
+		public string Decrypt(string plainText)
+		{
+			try
+			{
+                // Base 64 decode
+                byte[] base64Decoded = Convert.FromBase64String(plainText);
+				var base64DecodedStr = _encoding.GetString(base64Decoded);
+
+				// JSON Decode base64Str
+				var payload = JsonConvert.DeserializeObject<Dictionary<string, string>>(base64DecodedStr);
+
+				_aes.IV = Convert.FromBase64String(payload["iv"]);
+
+				ICryptoTransform AESDecrypt = _aes.CreateDecryptor(_aes.Key, _aes.IV);
+				byte[] buffer = Convert.FromBase64String(payload["value"]);
+
+				return _encoding.GetString(AESDecrypt.TransformFinalBlock(buffer, 0, buffer.Length));
+			}
+			catch (Exception e)
+			{
+			    throw new Exception("Error decrypting: " + e.Message);
+			}
+		}
+
+		private byte[] HmacSHA256(string data)
+		{
+			using (var hmac = new HMACSHA256(_aes.Key))
+			{
+				return hmac.ComputeHash(_encoding.GetBytes(data));
+			}
+		}
+	}
+} 
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CryptoEmulator
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            // The sample text to encrypt and decrypt.
+            const string Text = "121212";
+
+            var aes256CbcEncryptionService = new Aes256CbcEncryptionService(Key);
+            // Encrypt and decrypt the sample text via the Aes256CbcEncrypter class.
+            var Encrypted = aes256CbcEncryptionService.Encrypt(Text);
+            var Decrypted = aes256CbcEncryptionService.Decrypt(Encrypted);
+
+            Console.WriteLine("Encrypted: {0}", Encrypted);
+            Console.WriteLine("DB field:  {0}", DB_FIELD);
+
+            Console.ReadLine();
+        }
+    }
+}
+
