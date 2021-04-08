ae6197cd164fbcfe66e3589a491c4326b726b719
savaged <david.savage@assure-consulting.co.uk>
2021-04-08 10:32:51 +0100

    doco

--- ExampleClient/Program.cs
+++ ExampleClient/Program.cs
@@ -1,4 +1,16 @@
-﻿using IlluminateEncrypterEmulation;
+﻿/*
+ * Add a partial named `Program.partial.cs` with the following contents
+namespace ExampleClient
+{
+    partial class Program
+    {
+        public const string KEY = "your key here";
+
+        public const string DB_FIELD = "eyJpdiI6ImZVRm1ObFwvd29BUXBBbnVicnNTajB3PT0iLCJ2YWx1ZSI6Ik0wYU9qeU44aUM1YXRQZUZwSGlhVWc9PSIsIm1hYyI6IjQ4ODUwYjAzMzk4OTlkYTllM2U3MGExYjU3OTM5M2UzMzViYTZkMDFjYWE1ZjA0YzRiMGYxNjMxMjEwMzE1ODgifQ==";
+    }
+}
+ */
+using IlluminateEncrypterEmulation;
 using System;
 
 namespace ExampleClient
