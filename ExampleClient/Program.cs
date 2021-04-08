cf769a21670f96fb686dfee5ef0cd401388094a8
savaged <david.savage@assure-consulting.co.uk>
2021-04-08 10:29:37 +0100

    tidy

--- ExampleClient/Program.cs
+++ ExampleClient/Program.cs
@@ -1,19 +1,20 @@
-﻿using System;
+﻿using IlluminateEncrypterEmulation;
+using System;
 
-namespace CryptoEmulator
+namespace ExampleClient
 {
     partial class Program
     {
         static void Main(string[] args)
         {
-            var encrypter = new IlluminateEncrypterEmulator(KEY);
+            var encrypter = new Encrypter(KEY);
 
             var decrypted = encrypter.Decrypt(DB_FIELD);
 
             Console.WriteLine("Decrypted: {0}", decrypted);
 
 
-            var encrypted = encrypter.Encrypt("s:7:\"TP00202\";");
+            var encrypted = encrypter.Encrypt("s:6:\"121212\";");
             decrypted = encrypter.Decrypt(encrypted);
 
             Console.WriteLine("Decrypted: {0}", decrypted);
